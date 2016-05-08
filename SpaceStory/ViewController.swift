d6b94d77c6376fe62ed8f14815dabace2622c718
SimpleSols <danwilliams23.dw@gmail.com>
2016-05-08 13:19:24 +0100

    Initial Commit

---
+++ SpaceStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceStory
+//
+//  Created by User on 08/05/2016.
+//  Copyright © 2016 DanWilliams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
